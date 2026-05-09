7f78f6033992f9c80a26f31033ad2dee9bb26b3d
2mendevs <2mendevs@gmail.com>
2026-05-09 18:38:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import CoachingPage from "./pages/CoachingPage.tsx";
+import ProgramPage from "./pages/ProgramPage.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/coaching" element={<CoachingPage />} />
+          <Route path="/program" element={<ProgramPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
