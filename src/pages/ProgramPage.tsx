22afd262a87544f215235451d261b23da0e79cff
2mendevs <2mendevs@gmail.com>
2026-05-22 00:24:40 +0530

    Update ProgramPage.tsx

--- src/pages/ProgramPage.tsx
+++ src/pages/ProgramPage.tsx
@@ -2144,7 +2144,7 @@ const roadmapCards = [
 
             color: "#07b4ba",
             marginLeft: 160,
-            marginTop: -50,/* moves ₹7499 slightly right */
+            marginTop: -65,/* moves ₹7499 slightly right */
           }}
         >
           ₹7,499
