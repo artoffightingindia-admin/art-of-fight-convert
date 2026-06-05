e5fb41080a19b9827407d2e0b4fe1a31a5abb259
2mendevs <2mendevs@gmail.com>
2026-06-06 00:28:20 +0530

    Update IntroSection.tsx

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -5,7 +5,7 @@ const IntroSection = () => (
       {/* Section Caption */}
       <p
         className="text-[#07b4ba] font-['Barlow'] font-bold text-[14px] tracking-[4px] uppercase mb-3"
-        style={{ transform: "translateY(17px)" }}
+        style={{ transform: "translateY(25px)" }}
       >
         AOF Intro Section
       </p>
