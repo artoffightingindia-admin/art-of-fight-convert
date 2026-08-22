e982324756e1c4ae0a94f91e89ea28121419024e
2mendevs <2mendevs@gmail.com>
2026-08-22 16:21:57 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,35 +10,62 @@ import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 import Reveal from "@/components/Reveal";
 import StickyAd from "@/components/StickyAd";
+import { useCms } from "@/context/CmsContext";
 
-const Index = () => (
-  <div className="min-h-screen">
-    <StickyAd />
-    <Navbar />
-    <HeroSection />
-    <Reveal type="fade-up">
-      <IntroSection />
-    </Reveal>
-    <Reveal type="fade-up">
-      <ServicesSection />
-    </Reveal>
-    <Reveal type="fade-up">
-      <CoachSection />
-    </Reveal>
-    <Reveal type="scale-up">
-      <SocialProofSection />
-    </Reveal>
-    <Reveal type="fade-up">
-      <TestimonialsSection />
-    </Reveal>
-    <Reveal type="scale-up">
-      <CtaPairSection />
-    </Reveal>
-    <Reveal type="fade-up">
-      <ContactSection />
-    </Reveal>
-    <Footer />
-  </div>
-);
+const Index = () => {
+  const { content } = useCms();
+  const v = content.visibility;
+
+  return (
+    <div className="min-h-screen">
+      {v.stickyAd && <StickyAd />}
+      <Navbar />
+
+      {v.heroSection && <HeroSection />}
+
+      {v.introSection && (
+        <Reveal type="fade-up">
+          <IntroSection />
+        </Reveal>
+      )}
+
+      {v.servicesSection && (
+        <Reveal type="fade-up">
+          <ServicesSection />
+        </Reveal>
+      )}
+
+      {v.coachSection && (
+        <Reveal type="fade-up">
+          <CoachSection />
+        </Reveal>
+      )}
+
+      {v.socialProofSection && (
+        <Reveal type="scale-up">
+          <SocialProofSection />
+        </Reveal>
+      )}
+
+      {v.testimonialsSection && (
+        <Reveal type="fade-up">
+          <TestimonialsSection />
+        </Reveal>
+      )}
+
+      {v.ctaPairSection && (
+        <Reveal type="scale-up">
+          <CtaPairSection />
+        </Reveal>
+      )}
+
+      <Reveal type="fade-up">
+        <ContactSection />
+      </Reveal>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default Index;
