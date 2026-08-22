7b395842a551ba5ac71fd3494c054691926aa00c
2mendevs <2mendevs@gmail.com>
2026-08-22 13:46:57 +0530

    Update CmsContext.tsx

--- src/context/CmsContext.tsx
+++ src/context/CmsContext.tsx
@@ -1,4 +1,4 @@
-aimport React, { createContext, useContext, useState, useEffect, ReactNode } from "react";
+import React, { createContext, useContext, useState, useEffect, ReactNode } from "react";
 
 export interface SiteContent {
   heroTitle: string;
