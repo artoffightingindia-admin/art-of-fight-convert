4b0eb95774bec7c8dfe6c7d7dbe54184250b042c
2mendevs <2mendevs@gmail.com>
2026-08-23 11:17:59 +0530

    Update CtaPairSection.tsx

--- src/components/CtaPairSection.tsx
+++ src/components/CtaPairSection.tsx
@@ -147,6 +147,7 @@ const CtaPairSection = () => {
                   <line x1="8" y1="2" x2="8" y2="6" />
                   <line x1="3" y1="10" x2="21" y2="10" />
                 </svg>
+
                 {c.ctaCard2Title || "30-Days Program"}
               </span>
 
